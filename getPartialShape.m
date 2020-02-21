a3b7c112d45bc07b6f5141782d5fd69ef2db6c8a
tsachiblau <tsachiblau@gmail.com>
2020-02-21 22:25:51 +0200

    working version with the premitive optimization

--- getPartialShape.m
+++ getPartialShape.m
@@ -6,7 +6,7 @@ function [Y] = getPartialShape(X, num_of_polygons, seed_num)
     remaining_polygon_list = remaining_polygon_list(~ismember(remaining_polygon_list, taken_polygon_list));
     vertics_list = [X.TRIV(taken_polygon_list, :)]';
     
-    while size(taken_polygon_list, 2) < taken_polygon_list
+    while size(taken_polygon_list, 2) < num_of_polygons
         relevant_polygons = sum( ismember(X.TRIV(remaining_polygon_list, :), vertics_list), 2);
         [B, I] = sort(relevant_polygons, 'descend');
         neighbors = B >= 2;
@@ -16,11 +16,26 @@ function [Y] = getPartialShape(X, num_of_polygons, seed_num)
         remaining_polygon_list = remaining_polygon_list(~ismember(remaining_polygon_list, taken_polygon_list));
     end
     
-    Y.n = X.n;
+    tmp = X.TRIV(taken_polygon_list, :);
+    new_TRIV = [];
+    idx_of_vertex_in_original_array = sort(unique(vertics_list));
+    for i = 1 : size(taken_polygon_list, 2)
+        new_row = [];
+        for j = 1 : 3
+            convert_idx = find(idx_of_vertex_in_original_array == tmp(i, j));
+            new_row = [new_row, convert_idx];
+        end
+        new_TRIV = [new_TRIV; new_row];
+    end
+            
+    Y.n = size(idx_of_vertex_in_original_array, 1);
     Y.m = size(taken_polygon_list, 2);
     Y.indexes = 1 : size(taken_polygon_list, 2);
-    Y.VERT = X.VERT;
-    Y.TRIV = X.TRIV(taken_polygon_list, :);
+    Y.VERT = X.VERT(idx_of_vertex_in_original_array, :);
+    Y.TRIV = new_TRIV;
+    
+    Y.ORIGINAL_VERT = X.VERT;
+    Y.ORIGINAL_TRIV = X.TRIV(taken_polygon_list, :);
     
 end
 
--- main.m
+++ main.m
@@ -7,8 +7,8 @@ X = load_off(file_path);
 [laplace_X.W, ~, laplace_X.A] = calc_LB_FEM_bc(X, 'dirichlet');
 
 %% cut part of the shape
-num_of_polygons = 100;
-seed = 1;
+num_of_polygons = 500;
+seed = 5;
 Y = getPartialShape(X, num_of_polygons, seed);
 [laplace_Y.W, ~, laplace_Y.A] = calc_LB_FEM_bc(Y, 'dirichlet');
 
@@ -22,8 +22,8 @@ title('show patch');
 
 number_of_eig = 20;
 [laplace_X.eigenvectors, laplace_X.eigenvalue] = eigs(laplace_X.W, laplace_X.A, number_of_eig, 'SM');
-[~, laplace_Y.eigenvalue] = eigs(laplace_Y.W, laplace_Y.A, number_of_eig , 'SM');
-%%
+[laplace_Y.eigenvectors, laplace_Y.eigenvalue] = eigs(laplace_Y.W, laplace_Y.A, number_of_eig, 'SM');
+%% show eigenvalue on complete shape
 figure();
 i = 1;
 for row = 1 : 2
@@ -35,4 +35,80 @@ for row = 1 : 2
     end 
 end
 
+%% show eigenvalue on complete shape
+figure();
+i = 1;
+for row = 1 : 2
+    for col = 1 : 10
+        subplot(2, 10, i);
+        patch('Faces',Y.TRIV,'Vertices',Y.VERT, 'FaceVertexCData',laplace_Y.eigenvectors(:,i),'FaceColor','flat');
+        title(['eigenvalue number', num2str(i) ,'   is', num2str(laplace_Y.eigenvalue(i, i))]);
+        i = i + 1;
+    end 
+end
+
+
 %% show eigenvalue graph
+figure();
+plot( 1 : size(laplace_X.eigenvalue, 2), diag(laplace_X.eigenvalue), 'r');
+hold on;
+plot( 1 : size(laplace_Y.eigenvalue, 2), diag(laplace_Y.eigenvalue), 'b');
+legend('X', 'Y');
+
+%% optimization 
+eigenvalue_error = 2;
+draw_th = 0;
+eigenvalue_error_th = 1e-3;
+tau = 10 * laplace_Y.eigenvalue(end);
+v = ones(size(X.VERT, 1), 1) * (tau / 4);
+alpha = 1e-4;
+mu = diag(laplace_Y.eigenvalue);
+iter = 1;
+error_list = [];
+
+f = figure();
+while eigenvalue_error > eigenvalue_error_th
+    ex1 = sparse(laplace_X.W + laplace_X.A * diag(v));
+    [eigenvectors, eigenvalue] = eigs(ex1 , laplace_X.A, number_of_eig, 'SM');
+    ex2 = eigenvectors .* eigenvectors;
+    ex3 = (diag(eigenvalue) - mu) ./ (mu .^ 2);
+    ex4 = 2 * alpha * ex2 * ex3;
+    v = v - max(ex4, 0);
+    
+    eigenvalue_error = norm(diag(eigenvalue) - mu);
+    error_list = [error_list, eigenvalue_error];
+    
+    if mod(iter, 100) == 0
+        clf(f);
+        subplot(2, 2, 1);
+        plot(1:size(error_list, 2), error_list);
+        title('eigenvalue norm error');
+        subplot(2, 2, 3);
+        scatter(1:size(v, 1), v);
+        
+        idx_to_draw = v < draw_th;
+        subplot(2, 2, [2, 4]);
+
+        patch('Faces',X.TRIV,'Vertices',X.VERT, 'FaceColor', 'blue');
+        hold on;
+        patch('Faces',Y.TRIV,'Vertices',Y.VERT, 'FaceColor', 'green');
+        hold on;
+        scatter3(X.VERT(idx_to_draw,1), X.VERT(idx_to_draw,2), X.VERT(idx_to_draw,3), 'r', 'filled');
+        title('show patch');
+        pause(0.001);
+    end
+    
+    iter = iter + 1;
+end
+
+
+
+
+
+
+
+
+
+
+
+
